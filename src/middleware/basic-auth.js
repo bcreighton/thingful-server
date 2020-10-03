28b586436bec9e27f29688a649dfdf3652891de7
Bryson <brysoncreighton@gmail.com>
2020-10-02 23:49:56 -0600

    Begin implmenetation of basic authorization
    Created middleware to handles sending authorization to the server.

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,9 @@
+const requireAuth = (req, res, next) => {
+    const authToken = req.get('Authorization') || ''
+
+   if (!authToken.toLowerCase().startsWith('basic ')) {
+     return res.status(401).json({ error: 'Missing basic token' })
+   }
+
+    next()
+}
